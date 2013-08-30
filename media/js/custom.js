15b74293ae18d0e47d2a6cb2d1525b87a79c0b30
Eugene Shim <twoxmachine@gmail.com>
2013-08-30 18:14:17 -0400

    localscroll + skills formatting

--- media/js/custom.js
+++ media/js/custom.js
@@ -1,46 +1,5 @@
-//Carousel functions not currenty in use.
-
-
-/* DISABLE CAROUSEL AUTOCYCLING */
-$('#myCarousel').carousel({
-    interval: false
+jQuery(document).ready(function(){
+	// A lot of jquery plugins for this effect...
+	$.localScroll();
+	
 });
-
-
-// taken from http://jsfiddle.net/juddlyon/Q2TYv/10/
-/* SLIDE ON CLICK */ 
-
-$('.nav > li > a').click(function() {
-
-    // grab href, remove pound sign, convert to number
-    var item = Number($(this).attr('href').substring(1));
-
-    // slide to number -1 (account for zero indexing)
-    $('#myCarousel').carousel(item - 1);
-
-    // remove current active class
-    $('.nav .active').removeClass('active');
-
-    // add active class to just clicked on item
-    $(this).parent().addClass('active');
-
-    // don't follow the link
-    return false;
-});
-
-
-/* AUTOPLAY NAV HIGHLIGHT */
-
-// bind 'slid' function
-$('#myCarousel').bind('slid', function() {
-
-    // remove active class
-    $('.nav .active').removeClass('active');
-
-    // get index of currently active item
-    var idx = $('#myCarousel .item.active').index();
-
-    // select currently active item and add active class
-    $('.nav li:eq(' + idx + ')').addClass('active');
-
-});             
